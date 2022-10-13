5f461d6a1fcc62d19a455ab2928e7b56e96571c1
shoomonperry <shoomonperry@gmail.com>
2022-10-13 12:00:20 +0100

    added image folder with assets and code to allow access

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+import os
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
 async def root():
@@ -12,3 +15,13 @@ async def root():
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
 
+#get list of images
+@app.get("/images")
+def images():
+    out = []
+    for filename in os.listdir("static/images"):
+        out.append({
+            "name": filename.split(".")[0],
+            "path": "/static/images/" + filename
+        })
+    return out
